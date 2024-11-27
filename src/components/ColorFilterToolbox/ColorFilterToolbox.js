08ecd13df287d4319db7726660aac751512b6710
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2024-11-27 14:28:02 +0200

    created to work all toolbox filters

--- src/components/ColorFilterToolbox/ColorFilterToolbox.js
+++ src/components/ColorFilterToolbox/ColorFilterToolbox.js
@@ -1,9 +1,10 @@
 import { useEffect, useState, useRef } from 'react';
 import arrowDown from '../../data/images/icon/arrow-down-black.svg'
 
-const ColorFilterToolbox = ({data, filterToolboxes}) => {
+const ColorFilterToolbox = ({data, updateFilter}) => {
   const [colorsData, setColorsData] = useState([]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [currentColor, setCurrentColor] = useState('all');
 
   const menuRef = useRef(null);
 
@@ -34,27 +35,39 @@ const ColorFilterToolbox = ({data, filterToolboxes}) => {
     }
   })
 
+  const changeCurrentColor = (color) => {
+    setCurrentColor(color);
+    setIsMenuOpen(false);
+
+    updateFilter(color);
+  }
+
   return (
       <div ref={menuRef} className="filter-boxes__color filter-color" onClick={toggleMenu}>
-      <div className="filter-color__top d-flex align-items-center justify-content-center">
-        <div className="filter-color__text">Color:</div>
-        <div className="filter-color__view filter-color__view-all"></div>
-        <div className="filter-color__close open"><img src={arrowDown} alt="Open"/></div>
-      </div>
-      <div className={`filter-color__down ${isMenuOpen ? 'open' : 'close'}`}>
-        <div className="filter-color__item d-flex justify-content-center justify-content-sm-between align-items-center selected">
-          <div className="filter-color__text">All</div>
-          <div className="filter-color__view filter-color__view-all"></div>
+        <div className="filter-color__top d-flex align-items-center justify-content-center">
+          <div className="filter-color__text">Color:</div>
+          <div className={`filter-color__view filter-color__view-${currentColor}`}></div>
+          <div className="filter-color__close open"><img src={arrowDown} alt="Open"/></div>
         </div>
-        {colorsData.map(i => {
-          return (
-            <div key={i[0]} className="filter-color__item d-flex justify-content-center justify-content-sm-between align-items-center">
-              <div className="filter-color__text">{i[0]}</div>
-              <div className={`filter-color__view filter-color__view-${i[0]}`}></div>
-            </div>
-          )
-        })}
-      </div>
+        <div className={`filter-color__down ${isMenuOpen ? 'open' : 'close'}`}>
+          <div 
+            className="filter-color__item d-flex justify-content-center justify-content-sm-between align-items-center selected"
+            onClick={() => changeCurrentColor('all')}>
+              <div className="filter-color__text">All</div>
+              <div className={`filter-color__view filter-color__view-all`}></div>
+          </div>
+          {colorsData.map(i => {
+            return (
+              <div 
+                key={i[0]}
+                className="filter-color__item d-flex justify-content-center justify-content-sm-between align-items-center"
+                onClick={() => changeCurrentColor(i[0])}>
+                  <div className="filter-color__text">{i[0]}</div>
+                  <div className={`filter-color__view filter-color__view-${i[0]}`}></div>
+              </div>
+            )
+          })}
+        </div>
     </div>
   )
 }
