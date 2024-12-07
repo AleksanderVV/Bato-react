d727dac2f65e425e56de8c3ee66c142a9fb46421
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2024-12-07 15:46:33 +0200

    added Drawers bar and Acc

--- src/components/AccessoriesList/AccessoriesList.js
+++ src/components/AccessoriesList/AccessoriesList.js
@@ -4,6 +4,7 @@ import accImageSmall from '../../data/images/accessory-1-small.png';
 import plusBlack from '../../data/images/icon/plus-black.svg';
 import plusImage from '../../data/images/icon/plus.svg';
 import accSize1 from '../../data/images/icon/accessory-size-1.svg';
+import d8909 from '../../data/images/d-8909.jpg';
 
 const AccessoriesList = () => {
 
@@ -13,6 +14,38 @@ const AccessoriesList = () => {
                 <div className="tab-pane show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab" tabIndex="0">
                     <div className="choose-accessories__cards accessory-cards d-flex flex-wrap justify-content-left">
                         <div className="accessory-cards__item d-flex flex-column">
+                            <div className="accessory-cards__item_first">
+                                <div className="accessory-cards__img">
+                                    <div className="accessory-cards__img-wrapper">
+                                        <img src={d8909} alt="Accessory" className="d-none d-sm-inline" />
+                                        <img src={accImageSmall} alt="Accessory" className="d-inline d-sm-none" />
+                                    </div>
+                                </div>
+                                <div className="accessory-cards__content d-flex flex-column justify-content-between">
+                                    <div className="accessory-cards__title">BATO Socket wrench set 1/2" 6 edge 10-36mm.</div>
+                                    <div className="accessory-cards__main">
+                                        <div className="accessory-cards__id-accessory d-flex justify-content-between">
+                                            <p className="accessory-cards__id-subtitle">Item number:</p>
+                                            <p className="accessory-cards__id">8901</p>
+                                        </div>
+                                        <div className="accessory-cards__size-accessory d-flex justify-content-between">
+                                            <p className="accessory-cards__size-subtitle">Size <span>1</span>/3</p>
+                                            <p className="accessory-cards__size">
+                                                <img src={accSize1} className="accessory-cards__size-img" alt="size" data-size="1" />
+                                            </p>
+                                        </div>
+                                        <div className="accessory-cards__footer d-flex justify-content-between align-items-center">
+                                            <button type="button" className="d-flex align-items-center justify-content-center">
+                                                <img src={plusBlack} alt="plus" className="accessory-cards__plus" />
+                                                <img src={plusImage} alt="plus" className="accessory-cards__plus-hover" />
+                                            </button>
+                                            <p className="accessory-cards__price"><span>121</span>,00 EUR</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accessory-cards__item_second">
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/DrawerSideBar/DrawerSideBar.js
+++ src/components/DrawerSideBar/DrawerSideBar.js
@@ -1,6 +1,86 @@
 import './drawerSideBar.scss';
 
+import xIcon from '../../data/images/icon/x-icon.svg';
+import drawer3 from '../../data/images/drawer3.webp';
+import resetImage from '../../data/images/icon/reset.svg';
+
 const DrawerSideBar = ({currentToolbox, totalPrice}) => {
+
+    const drawersNavigation = () => {
+        return (
+            <>
+                <p className="d-none d-sm-block">Drawer</p>
+                <button className="nav-link d-flex align-items-center active" id="v-pills-1-tab" data-bs-toggle="pill" data-bs-target="#v-pills-1" type="button" role="tab" aria-controls="v-pills-1" aria-selected="true">
+                    <div className="choose-accessories__nav-img nav-img d-flex">
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                    </div>
+                    <span className="button-number">01</span>
+                    <span className="d-sm-none me-1">Drawer</span>
+                </button>
+                <button className="nav-link d-flex align-items-center" id="v-pills-2-tab" data-bs-toggle="pill" data-bs-target="#v-pills-2" type="button" role="tab" aria-controls="v-pills-2" aria-selected="false" tabIndex="-1">
+                    <div className="choose-accessories__nav-img nav-img d-flex">
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                    </div>
+                    <span className="button-number">02</span>
+                    <span className="d-sm-none me-1">Drawer</span>
+                </button>
+                <button className="nav-link d-flex align-items-center" id="v-pills-3-tab" data-bs-toggle="pill" data-bs-target="#v-pills-3" type="button" role="tab" aria-controls="v-pills-3" aria-selected="false" tabIndex="-1">
+                    <div className="choose-accessories__nav-img nav-img d-flex">
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                    </div>
+                    <span className="button-number">03</span>
+                    <span className="d-sm-none me-1">Drawer</span>
+                </button>
+                <button className="nav-link d-flex align-items-center" id="v-pills-4-tab" data-bs-toggle="pill" data-bs-target="#v-pills-4" type="button" role="tab" aria-controls="v-pills-4" aria-selected="false" tabIndex="-1">
+                    <div className="choose-accessories__nav-img nav-img d-flex">
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                        <div className="nav-img__item"></div>
+                    </div>
+                    <span className="button-number">04</span>
+                    <span className="d-sm-none me-1">Drawer</span>
+                </button>
+                <div className="nav-list_top d-sm-none d-flex justify-content-center align-items-center"></div>
+                <div className="nav-list_bottom d-sm-none d-flex justify-content-center align-items-center"></div>
+                <div className="d-sm-none">
+                    <select id="mobileTabsSelect"><option value="1">Drawer 1</option><option value="2">Drawer 2</option><option value="3">Drawer 3</option><option value="4">Drawer 4</option></select>
+                </div>
+            </>
+        )
+    }
+
+    const drawersView = () => {
+        return (
+            <>
+                <div className="tab-pane active show" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab" tabIndex="0">
+                    <div className="choose-accessories__drawers-content drawers-content"></div>
+                    <img src={drawer3} alt="Shelf" />
+                    <p className="d-flex align-items-center not-active"><img src={resetImage} alt="" />Reset</p>
+                </div>
+                <div className="tab-pane" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-2-tab" tabIndex="0">
+                    <div className="choose-accessories__drawers-content drawers-content"></div>
+                    <img src={drawer3} alt="Shelf" />
+                    <p className="d-flex align-items-center not-active"><img src={resetImage} alt="" />Reset</p>
+                </div>
+                <div className="tab-pane" id="v-pills-3" role="tabpanel" aria-labelledby="v-pills-3-tab" tabIndex="0">
+                    <div className="choose-accessories__drawers-content drawers-content"></div>
+                    <img src={drawer3} alt="Shelf" />
+                    <p className="d-flex align-items-center not-active"><img src={resetImage} alt="" />Reset</p>
+                </div>
+                <div className="tab-pane" id="v-pills-4" role="tabpanel" aria-labelledby="v-pills-4-tab" tabIndex="0">
+                    <div className="choose-accessories__drawers-content drawers-content"></div>
+                    <img src={drawer3} alt="Shelf" />
+                    <p className="d-flex align-items-center not-active"><img src={resetImage} alt="" />Reset</p>
+                </div>
+            </>
+        )
+    }
 
     return (
         <div className="col-xl-6 col-xxl-4">
@@ -9,38 +89,40 @@ const DrawerSideBar = ({currentToolbox, totalPrice}) => {
             </div>
             <div className="choose-accessories__drawers">
                 <div className="choose-accessories__close-popup d-sm-none d-flex justify-content-center align-items-center">
-                <img src="img/icon/x-icon.svg" alt="close" />
+                    <img src={xIcon} alt="close" />
                 </div>
                 <div className="choose-accessories__drawers-tabs">
-                <div className="choose-accessories__drawers-wrapper d-flex align-items-start justify-content-center justify-content-xl-start">
-                    <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <div className="choose-accessories__drawers-wrapper d-flex align-items-start justify-content-center justify-content-xl-start">
+                        <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            {drawersNavigation()}
+                        </div>
+                        <div className="tab-content" id="v-pills-tabContent">
+                            {drawersView()}
+                        </div>
                     </div>
-                    <div className="tab-content" id="v-pills-tabContent">
-                    </div>
-                </div>
                 </div>
                 <div className="choose-accessories__drawers-price-total">
-                <div className="choose-accessories__drawers-price-title d-flex justify-content-between">
-                    <p>
-                    Total price
-                    </p>
-                    <p className="choose-accessories__drawers-price-summ">
-                    <span>{totalPrice}</span>,00 EUR
-                    </p>
-                </div>
-                <div className="choose-accessories__drawers-price-info d-flex justify-content-between">
-                    <div className="choose-accessories__drawers-price-button align-items-center justify-content-center d-none d-sm-flex">
-                    <a><span><img src="img/icon/cart.svg" alt="" /> Færdig med valg</span></a>
+                    <div className="choose-accessories__drawers-price-title d-flex justify-content-between">
+                        <p>
+                        Total price
+                        </p>
+                        <p className="choose-accessories__drawers-price-summ">
+                        <span>{totalPrice}</span>,00 EUR
+                        </p>
                     </div>
-                    <div className="choose-accessories__drawers-price-items text-end">
-                    <div className="choose-accessories__drawers-price-quantity">
-                        <span>0</span> items added
+                    <div className="choose-accessories__drawers-price-info d-flex justify-content-between">
+                        <div className="choose-accessories__drawers-price-button align-items-center justify-content-center d-none d-sm-flex">
+                        <a><span><img src="img/icon/cart.svg" alt="" /> Færdig med valg</span></a>
+                        </div>
+                        <div className="choose-accessories__drawers-price-items text-end">
+                        <div className="choose-accessories__drawers-price-quantity">
+                            <span>0</span> items added
+                        </div>
+                        <div className="choose-accessories__drawers-price-show">
+                            Show added items
+                        </div>
+                        </div>
                     </div>
-                    <div className="choose-accessories__drawers-price-show">
-                        Show added items
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
             <div className="choose-accessories__drawers-price-button2 align-items-center justify-content-center d-sm-none">
