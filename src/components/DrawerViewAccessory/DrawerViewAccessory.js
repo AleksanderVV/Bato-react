dd5701714fdda0e1a54d18de39e6c0e5a87e1521
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-01-14 14:18:33 +0200

    import images to component

--- src/components/DrawerViewAccessory/DrawerViewAccessory.js
+++ src/components/DrawerViewAccessory/DrawerViewAccessory.js
@@ -1,4 +1,6 @@
-
+import accSize1Image from '../../data/images/accessory-size-1.jpg';
+import accSize2Image from '../../data/images/accessory-size-2.jpg';
+import accSize3Image from '../../data/images/accessory-size-3.jpg';
 
 const DrawerViewAccessory = () => {
 
