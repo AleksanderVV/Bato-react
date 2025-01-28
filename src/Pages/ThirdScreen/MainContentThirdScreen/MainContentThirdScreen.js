83c87eb8a224b8a1e9ae3d9f99ffe9a8146e6920
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-01-28 14:03:43 +0200

    created form

--- src/Pages/ThirdScreen/MainContentThirdScreen/MainContentThirdScreen.js
+++ src/Pages/ThirdScreen/MainContentThirdScreen/MainContentThirdScreen.js
@@ -1,4 +1,6 @@
 import {v4 as uuidv4} from 'uuid';
+import { useFormik } from 'formik';
+
 import './mainContentThirdScreen.scss';
 
 import mailSendImage from '../../../data/images/icon/mail-send.svg';
@@ -20,29 +22,7 @@ const MainContentThirdScreen = ({
                 <div className="col-12 order-2 col-md-6 order-md-1 d-flex align-items-center">
                 <div className="total-result__form-box">
                     <h3>Get a offer for the selected toolbox and accessories</h3>
-                    <form action="#">
-                        <p>
-                            <input type="text" name="name" id="name" placeholder="Name and Surname" />
-                        </p>
-                        <p>
-                            <input type="text" name="company" id="company" placeholder="Company" />
-                        </p>
-                        <p>
-                            <input type="text" name="zip" id="zip" placeholder="Zip Code" />
-                        </p>
-                        <p>
-                            <input type="text" name="email" id="email" placeholder="E-mail address" />
-                        </p>
-                        <p>
-                            <input type="text" name="phone" id="phone" placeholder="Telephone" />
-                        </p>
-                        <p>
-                            <textarea name="message" id="message" placeholder="Message"></textarea>
-                        </p>
-                        <p className="total-result__submit">
-                            <button type="submit"><span>Ja tak, send mig et tilbud</span></button>
-                        </p>
-                    </form>
+                    <Form />
                 </div>
                 <div className="total-result__after-send">
                     <img src={mailSendImage} alt="Email" className="d-none d-md-inline" />
@@ -112,5 +92,71 @@ const ListAccessories = ({drawersData, selectedAttachedAcc}) => {
                 })}
     </>
 };
+
+const Form = () => {
+
+    const validate = values => {
+        const errors = {};
+
+        if (!values.name) {
+            errors.name = 'Required';
+        } else if (values.name.length < 2) {
+            errors.name = 'Minimum 2 letters length!!!'
+        }
+
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+
+        return errors;
+    }
+
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            company: '',
+            zip: '',
+            email: '',
+            phone: '',
+            message: ''
+        },
+        validate,
+        onSubmit: values => console.log(JSON.stringify(values, null, 2))
+    })
+
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <p>
+                <input type="text" name="name" id="name" placeholder="Name and Surname" value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                {formik.errors.name && formik.touched.name ? <span style={{color: 'red'}}>{formik.errors.name}</span> : null}
+            </p>
+            <p>
+                <input type="text" name="company" id="company" placeholder="Company" value={formik.values.company} onChange={formik.handleChange} />
+            </p>
+            <p>
+                <input type="text" name="zip" id="zip" placeholder="Zip Code" value={formik.values.zip} onChange={formik.handleChange} />
+            </p>
+            <p>
+                <input type="text" name="email" id="email" placeholder="E-mail address" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                {formik.errors.email && formik.touched.email ? <span style={{color: 'red'}}>{formik.errors.email}</span> : null}
+            </p>
+            <p>
+                <input type="text" name="phone" id="phone" placeholder="Telephone" value={formik.values.phone} onChange={formik.handleChange} />
+            </p>
+            <p>
+                <textarea name="message" id="message" placeholder="Message" value={formik.values.message} onChange={formik.handleChange}></textarea>
+            </p>
+            <p className="total-result__submit">
+                <button type="submit"><span>Ja tak, send mig et tilbud</span></button>
+            </p>
+        </form>
+    )
+}
 
 export default MainContentThirdScreen;
